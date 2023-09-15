b848ceded2b4cbdc3ff78f924f0556e7b53fc14f
Deepaksoni7 <109645128+Deepaksoni7@users.noreply.github.com>
2023-09-15 16:45:37 +0530

    app.py

--- TESTInternalApplication/app.py
+++ TESTInternalApplication/app.py
@@ -118,4 +118,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     # app.run(debug=True)
-    app.run(debug=True)
+    app.run('app:app', host='0.0.0.0', port=1234)
